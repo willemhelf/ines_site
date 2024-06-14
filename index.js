b2fa9d04365b0774596f628efadae69bf49cee9d
Willem Helf <sophie.helf@gmail.com>
2024-06-14 13:03:43 -0400

    added event listeners to buttons

--- index.js
+++ index.js
@@ -1,26 +1,14 @@
-// array of assets
+window.addEventListener('load', function(){
+    const backgroundImages = ["assets/apak-note.png", "assets/gama-note.gif", "assets/ice-notes.gif", "assets/joes-notes.gif", "assets/milk_note.gif", "assets/mofu-note.gif", "assets/nekojiru2.png", "assets/nekojiru4.png", "assets/nekojiru8.png", "assets/nekojiru13.png", "assets/nekojiru24.png", "assets/nekojiru27.png","assets/nemurineko_NOTE.gif", "assets/nyan-note.gif"]
+    const buttons = document.querySelectorAll('.item')
+    const buttonsArray = Array.from(buttons)
+    console.log(buttonsArray)
 
-let backgroundImages = ["assets/apak-note.png", "assets/gama-note.gif", "assets/ice-notes.gif", "assets/joes-notes.gif", "assets/milk_note.gif", "assets/mofu-note.gif", "assets/nekojiru2.png", "assets/nekojiru4.png", "assets/nekojiru8.png", "assets/nekojiru13.png", "assets/nekojiru24.png", "assets/nekojiru27.png","assets/nemurineko_NOTE.gif", "assets/nyan-note.gif"]
-
-// test function
-// function colorChange() {
-//     let btn = document.getElementsByClassName('btn')[0]
-//     btn.addEventListener('click', function onClick() {
-//         document.querySelector('body').style.backgroundColor = "blue"
-//         console.log("test")
-//     })
-// }
-
-// change background on menu button click
-
-function backgroundChange() {
-    let buttons = document.getElementsByClassName('item')
-    for (let i = 0; i < buttons.length; i++) {
-        buttons[i].addEventListener('click', function() {
-            document.body.style.backgroundImage = `url(${backgroundImages[i]})`
-            console.log(document.body.style.backgroundImage)
+    for (var i = 0; i < buttonsArray.length; i++) {
+        buttonsArray[i].addEventListener("click", function() {
+            console.log("test")
         })
     }
-}
+})
 
-//`url(${backgroundImages[0]})`
+// `url(${backgroundImages[i]})`
