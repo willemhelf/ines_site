83dfd3f1af5765aab48011b2b05976e0cbf970cc
Willem Helf <sophie.helf@gmail.com>
2024-06-14 14:07:54 -0400

    resized css images

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ window.addEventListener('load', function(){
     const backgroundImages = ["assets/apak-note.png", "assets/gama-note.gif", "assets/ice-notes.gif", "assets/joes-notes.gif", "assets/milk_note.gif", "assets/mofu-note.gif", "assets/nekojiru2.png", "assets/nekojiru4.png", "assets/nekojiru8.png", "assets/nekojiru13.png", "assets/nekojiru24.png", "assets/nekojiru27.png","assets/nemurineko_NOTE.gif", "assets/nyan-note.gif"]
     const buttons = document.querySelectorAll('.item')
     const buttonsArray = Array.from(buttons)
+    let counter = 0;
 
-    for (var i = 0; i < buttonsArray.length - 1; i++) {
+    for (var i = 0; i < buttonsArray.length; i++) {
         buttonsArray[i].addEventListener("click", function() {
-            console.log(backgroundImages[i])
+            document.body.style.backgroundImage = `url(${backgroundImages[counter]})`
+            counter++
         })
     }
 })
-
-// `url(${backgroundImages[i]})`
