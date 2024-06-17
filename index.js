8f9fb36d6be89b7b9bc6e6528f240727a8e78a9f
Willem Helf <sophie.helf@gmail.com>
2024-06-17 13:24:31 -0400

    added menu dot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', function(){
     const backgroundImages = ["assets/apak-note.png", "assets/gama-note.gif", "assets/ice-notes.gif", "assets/joes-notes.gif", "assets/milk_note.gif", "assets/mofu-note.gif", "assets/nekojiru2.png", "assets/nekojiru4.png", "assets/nekojiru8.png", "assets/nekojiru13.png", "assets/nekojiru24.png", "assets/nekojiru27.png","assets/nemurineko_NOTE.gif", "assets/nyan-note.gif"]
-    const buttons = document.querySelectorAll('.item')
+    const buttons = document.querySelectorAll('.item:not(#menu-dot)')
     const buttonsArray = Array.from(buttons)
     let counter = 0;
 
