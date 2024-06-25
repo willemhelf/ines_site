fe0d6bdc9f6013b098d3219dc92d287efdbe981a
Willem Helf <sophie.helf@gmail.com>
2024-06-25 14:09:21 -0400

    added margins

--- mobile.js
+++ mobile.js
@@ -4,6 +4,7 @@ function displayDropdown() {
         element.style.display = 'flex'
         element.style['align-items'] = "center"
         element.style["z-index"] = 2 
+        element.style["margin-top"] = "11px"
       })
 
     console.log(Array.from(document.getElementsByClassName('hide')))
