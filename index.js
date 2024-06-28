c59ccca423a771807983c632c63a46b1e6df9408
Willem Helf <sophie.helf@gmail.com>
2024-06-28 15:47:06 -0400

    completely retooling js for correct background

--- index.js
+++ index.js
@@ -2,12 +2,55 @@ window.addEventListener('load', function(){
     const backgroundImages = ["assets/apak-note.png", "assets/gama-note.gif", "assets/ice-notes.gif", "assets/joes-notes.gif", "assets/milk_note.gif", "assets/mofu-note.gif", "assets/nekojiru2.png", "assets/nekojiru4.png", "assets/nekojiru8.png", "assets/nekojiru13.png", "assets/nekojiru24.png", "assets/nekojiru27.png","assets/nemurineko_NOTE.gif", "assets/nyan-note.gif"]
     const buttons = document.querySelectorAll('.item:not(#menu-dot)')
     const buttonsArray = Array.from(buttons)
-    let counter = 0;
-
-    for (var i = 0; i < buttonsArray.length; i++) {
-        buttonsArray[i].addEventListener("click", function() {
-            document.body.style.backgroundImage = `url(${backgroundImages[counter]})`
-            counter++
+    
+    buttonsArray.forEach(function(item) {
+        item.addEventListener('click', function() {
+            obj[this.id]()
         })
+    })
+
+    var obj = {
+        "apak-note": function() {
+            document.body.style.backgroundImage = "url(assets/apak-note.png)"
+        },
+        "gama-note" : function() {
+            document.body.style.backgroundImage = "url(assets/gama-note.gif)"
+        },
+        "ice-notes": function() {
+            document.body.style.backgroundImage = "url(assets/ice-notes.gif)"
+        },
+        "joes-notes": function() {
+            document.body.style.backgroundImage = "url(assets/joes-notes.gif)"
+        },
+        "milk_note" : function() {
+            document.body.style.backgroundImage = "url(assets/milk_note.gif)"
+        },
+        "mofu-note": function() {
+            document.body.style.backgroundImage = "url(assets/mofu-note.gif)"
+        },
+        "nekojiru2": function() {
+            document.body.style.backgroundImage = "url(assets/nekojiru2.png)"
+        },
+        "nekojiru4": function() {
+            document.body.style.backgroundImage = "url(assets/nekojiru4.png)"
+        },
+        "nekojiru8": function() {
+            document.body.style.backgroundImage = "url(assets/nekojiru8.png)"
+        },
+        "nekojiru13": function() {
+            document.body.style.backgroundImage = "url(assets/nekojiru13.png)"
+        },
+        "nekojiru24": function() {
+            document.body.style.backgroundImage = "url(assets/nekojiru24.png)"
+        },
+        "nekojiru27": function() {
+            document.body.style.backgroundImage = "url(assets/nekojiru27.png)"
+        },
+        "nemurineko_NOTE": function() {
+            document.body.style.backgroundImage = "url(assets/nemurineko_NOTE.gif)"
+        },
+        "nyan-note": function() {
+            document.body.style.backgroundImage = "url(assets/nyan-note.gif)"
+        }
     }
 })
